6c38d05341481b60de74ca11f444280642ff8501
NavpreetDevpuri <NavpreetDevpuri@gmail.com>
2020-11-06 20:33:15 +0530

    Update messenger.py

--- tithif/messenger.py
+++ tithif/messenger.py
@@ -8,11 +8,11 @@ class Messenger(FBObject):
         super().__init__(browser)
 
 
-    def open_chat_to(self, nameorusername):
+    def open_chat_to_fb_numeric_id(self, number):
         self._open_messenger_if_not_opened()
+        self.browser.get(f'https://www.facebook.com/messages/t/{number}')
 
 
     def _open_messenger_if_not_opened(self):
         if self.browser.current_url.find("www.facebook.com/messages") == -1:
-            self.browser.get("https://www.facebook.com/messages")
             self.browser.get("https://www.facebook.com/messages")
