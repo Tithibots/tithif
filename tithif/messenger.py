c92d717c894dd0ff4c6dfe2f26ba35091bbeb0f9
NavpreetDevpuri <NavpreetDevpuri@gmail.com>
2020-11-06 22:13:36 +0530

    Update messenger.py

--- tithif/messenger.py
+++ tithif/messenger.py
@@ -15,3 +15,13 @@ class Messenger(FBObject):
     def _open_messenger_if_not_opened(self):
         if self.browser.current_url.find("www.facebook.com/messages") == -1:
             self.browser.get("https://www.facebook.com/messages")
+
+    def open_chat_to_profile_url(self, url):
+        url=''
+        if url.find('?id=') == -1:
+            0
+        else:
+            0
+
+        #https://www.facebook.com/profile.php?id=100003803338568&sk=about
+        #https://www.facebook.com/navpreetdevpuri/
